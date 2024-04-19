0329bfca51e468cc30b30aabded5a2c7326065bf
Catherine Brett <catherinejanebrett@icloud.com>
2024-04-19 09:26:27 +0100

    Adds some homepage tsx - just showing a title atm

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,14 @@
 function Home() {
-   return <div>Home</div>
+   
+  return (
+    <section className="hero is-link is-fullheight-with-navbar">
+      <div className="hero-body has-text-centered">
+        <div className="container">
+          <p className="title">Harborne Community Groups</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
